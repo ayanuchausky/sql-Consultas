af8f7f87bb236c1fbc0cf85cf70b3cadbad1ba45
ayanuchausky <43687444+ayanuchausky@users.noreply.github.com>
2019-11-14 13:03:39 -0300

    Add files via upload

---
+++ EjercicioSQL/src/main/java/com/example/handlingformsubmission/service/ServiceApp.java
@@ -0,0 +1,57 @@
+package com.example.handlingformsubmission.service;
+
+import com.example.handlingformsubmission.Mappers.AirlinesRowMapper;
+import com.example.handlingformsubmission.model.Airline;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceApp {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    /**
+     *
+     * @return
+     */
+    public  List<Airline> getAirlines(){
+        String sql="select * from AIRLINES";
+        List<Airline> query = jdbcTemplate.query(sql, new AirlinesRowMapper());
+        query.forEach(item->System.out.println(item.getAirlineNameShort()));
+        return query;
+    }
+
+
+    /**
+     *
+     * @param shortName
+     * @param longName
+     * @return
+     */
+    public Integer getEconomySeatsFromAirlines(String shortName, String longName){
+        String query="select * from AIRLINES where AIRLINE=?  and AIRLINE_FULL=? ;";
+        List<Airline> airline= jdbcTemplate.query(query,new Object[]{shortName,longName},new AirlinesRowMapper());
+        if(!airline.isEmpty()){
+            Airline a= airline.get(0);
+            return a.getEconomySeat();
+        }
+        return 0;
+    }
+    public Integer getFirstClassSeatsFromAirlines(String shortName, String longName){
+        String query="select * from AIRLINES where AIRLINE=?  and AIRLINE_FULL=? ;";
+        List<Airline> airline= jdbcTemplate.query(query,new Object[]{shortName,longName},new AirlinesRowMapper());
+        System.out.println(airline);
+        if(!airline.isEmpty()){
+            Airline a= airline.get(0);
+            System.out.println("Hola buen dia entre");
+            return a.getFirstClassSeat();
+            
+        }
+        return 0;
+    }
+}
